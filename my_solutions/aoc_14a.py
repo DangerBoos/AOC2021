c84983ed46666014aedd76811f0c0b0ae007f5da
vselsov <vselsov@nanigans.com>
2021-12-14 13:19:24 -0500

    day14

--- my_solutions/aoc_14a.py
+++ my_solutions/aoc_14a.py
@@ -35,13 +35,12 @@ total_steps = 40
 template, __rules = dta.split('\n\n')
 _rules = [x.split(' -> ') for x in __rules.splitlines()]
 
-# mappning rules
+# mapping rules
 rules = defaultdict(lambda: [])
 dr = defaultdict(lambda: '')
 for i in range(len(_rules)):
     rules[_rules[i][0]] = [_rules[i][0][0] + _rules[i][1], _rules[i][1] + _rules[i][0][1]]
     dr[_rules[i][0]] = _rules[i][0][0] + _rules[i][1] + _rules[i][0][1]
-# i=2
 
 pair_counts = defaultdict(lambda: 0)
 # seed counts
@@ -54,7 +53,6 @@ for _ in range(total_steps):
         for new_element in rules[k]:
             new_pair_counts[new_element] += pair_counts[k]
     pair_counts = new_pair_counts.copy()
-    # print(_,sum(pair_counts.values()))
 
 approx_dbl_letter_counts = defaultdict(lambda: 0)
 pair_to_letter_map = dict()
@@ -66,12 +64,14 @@ for pair in list(pair_counts.keys()):
     for letter in pair_to_letter_map[pair]:
         approx_dbl_letter_counts[letter] += pair_counts[pair]
 
-approx_dbl_letter_counts[template[0]]+=1
-approx_dbl_letter_counts[template[-1]]+=1
+approx_dbl_letter_counts[template[0]] += 1
+approx_dbl_letter_counts[template[-1]] += 1
 
 letter_counts = dict()
 for k in approx_dbl_letter_counts:
-    letter_counts[k]=approx_dbl_letter_counts[k]/2
+    letter_counts[k] = approx_dbl_letter_counts[k] / 2
 
-letter_counts_sorted =  sorted(letter_counts.items(), key=lambda item: item[1])
-print(letter_counts_sorted[-1][1]-letter_counts_sorted[0][1])
+letter_counts_sorted = sorted(letter_counts.items(), key=lambda item: item[1])
+print(letter_counts_sorted[-1][1] - letter_counts_sorted[0][1])
+
+# lessons: sort to take max and min!
